299eeab26b12a9a5f789531304e3d9c3e4894735
Jessany Kaline <jessany42@gmail.com>
2022-11-06 18:41:15 -0300

    style and fonts

---
+++ react-lis-app/src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+export function Home() {
+  
+  return (
+    <div className="container">
+    <h1>Lembretes do Dia</h1>
+    <input type="text" placeholder="Digite a tarefa..."/>
+    <button typer="button">Adicionar</button>
+    </div>
+  )
+}
+
+
